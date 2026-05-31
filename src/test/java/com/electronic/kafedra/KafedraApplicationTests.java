eb79565b8f09d8881bd154430ba7b3b9162118b1
artur <mauboroda.arturtu@gmail.com>
2026-05-31 13:46:40 +0300

    Create project -connect to database postgreSQL -create 3 module -add application.yml

---
+++ src/test/java/com/electronic/kafedra/KafedraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electronic.kafedra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafedraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
